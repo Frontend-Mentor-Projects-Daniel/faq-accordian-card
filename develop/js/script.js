2890671ab17583955c1b7369a5053a176089d1b5
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-08-08 09:02:52 -0400

    use javascript to show box on tablet screen width and greater

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,4 +1,6 @@
 const faqQuestionBox = document.querySelectorAll('.faq__qna');
+const orangeBox = document.querySelector('[data-image-box]');
+const body = document.body;
 
 const handleClickFAQ = (e) => {
   // get only parent div when clicked
@@ -18,3 +20,14 @@ const handleClickFAQ = (e) => {
 faqQuestionBox.forEach((question) => {
   question.addEventListener('click', handleClickFAQ);
 });
+
+const observer = new ResizeObserver((entries) => {
+  const bodyElement = entries[0];
+  const tabletScreenSize = bodyElement.contentRect.width >= 768;
+
+  tabletScreenSize
+    ? orangeBox.classList.remove('hidden')
+    : orangeBox.classList.add('hidden');
+});
+
+observer.observe(body);
